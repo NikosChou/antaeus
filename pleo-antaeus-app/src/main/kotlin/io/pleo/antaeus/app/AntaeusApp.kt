bfa273a0c670bf8f7895460f1b6430631eaee5e0
choustni <nikolaos@choustoulakis.de>
2022-05-01 23:46:41 +0300

    inject BillingService to AntaeusRest

--- pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
+++ pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
@@ -62,11 +62,15 @@ fun main() {
     val customerService = CustomerService(dal = dal)
 
     // This is _your_ billing service to be included where you see fit
-    val billingService = BillingService(paymentProvider = paymentProvider)
+    val billingService = BillingService(
+        dal = dal,
+        paymentProvider = paymentProvider
+    )
 
     // Create REST web service
     AntaeusRest(
         invoiceService = invoiceService,
-        customerService = customerService
+        customerService = customerService,
+        billingService = billingService
     ).run()
 }
